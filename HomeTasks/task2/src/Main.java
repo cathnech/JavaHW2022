04f672993b9779353aedeab47a84dc13f02f71e7
cathnech <cathnech@gmail.com>
2022-09-03 05:32:34 +0200

    task2 add allWays

--- HomeTasks/task2/src/Main.java
+++ HomeTasks/task2/src/Main.java
@@ -13,7 +13,9 @@ public class Main {
 
 
     public static class constants {
-        public static int c = 2, d = 6;
+        public static int c = 1; // команда 1 (к1): увеличить в с раза, а умножается на c
+        public static int d = 2; // команда 2 (к2): увеличить на d , к a прибавляется d
+        //        public static int c = 5, d = 3;
         public static String mistake = " нет решения";
     }
 
@@ -26,27 +28,42 @@ public class Main {
 
         if ((a >= b) || ((b - a) < constants.d))
             System.out.println(constants.mistake);
-        else
+        else {
             System.out.println(bestWayForResult(a, b, ""));
+            System.out.println("Count of ways is " + allWays(a, b));
+        }
     }
 
     //функция, которая выдаёт набор команд, позволяющий число a превратить в число b
     static String bestWayForResult(int a, int b, String res) {
-        if (a == b) {
+        if (a == b)
             return String.valueOf(a);
-        }
 
-        if ((b % constants.c == 0) && (a <= b / constants.c)) {
+        if ((b % constants.c == 0) && (a <= b / constants.c) && (constants.c != 1)) {
             res += bestWayForResult(a, (b / constants.c), res);
-            if (res == constants.mistake) return constants.mistake;
-            return res + "*" + constants.c + "(cmd1)";
+            if (res.contentEquals(constants.mistake))
+                return constants.mistake;
+            else
+                return res + "*" + constants.c + "(cmd1)";
         } else if (a <= b - constants.d) {
             res += bestWayForResult(a, (b - constants.d), res);
-            if (res == constants.mistake) return constants.mistake;
-            return res + "+" + constants.d + "(cmd2)";
-        } else {
+            if (res.contentEquals(constants.mistake))
+                return constants.mistake;
+            else
+                return res + "+" + constants.d + "(cmd2)";
+        } else
             return constants.mistake;
-        }
+    }
+
+    //функция, которая выдаёт возможное количество путей, позволяющих число a превратить в число b
+    static int allWays(int a, int b) {
+        if (a == b)
+            return 1;
+        if ((b % constants.c == 0) && (constants.c != 1))
+            return allWays(a, b / constants.c) + allWays(a, b - constants.d);
+        if (a <= b - constants.d)
+            return allWays(a, b - constants.d);
+        else
+            return 0;
     }
 }
-
